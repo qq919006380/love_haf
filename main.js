196d98655d10986e6724e6f9a20c4f7046373065
weibaichao <919006380@qq.com>
2019-08-03 18:28:00 +0800

    add

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import util from '/util.js';
-console.log(util)
+var arr=[
+    {a:"asss",b:"asd"},
+    {a:"123",b:"213"},
+    {a:"ili",b:"lol"}
+]
+var obj={
+    'a':"a111",
+    'b':"b222"
+}
+var a=util.cahngeKeyAll(arr,obj)
+console.log(a)
